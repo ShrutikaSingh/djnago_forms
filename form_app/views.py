7f9fc4f3be9e95431ffbdc1389cc7f27a156363b
Shrutika <you@example.com>
2019-04-17 17:35:29 +0530

    completed bound form returning the submitted data to terminal

--- form_app/views.py
+++ form_app/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from .forms import ContactForm
 # Create your views here.
 def contact(request):
+    if request.method=='POST':
+        form=ContactForm(request.POST)#this will populate our form with the data submitted by user
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            name=form.cleaned_data['email']
+            
     form=ContactForm()
     #return HttpResponse('contact view') it was earlier when we dont have template
     return render(request, 'form.html', {'forms_11':form})
